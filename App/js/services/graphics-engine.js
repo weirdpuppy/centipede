36723fdcc6e34092ce2d5af12634dab1bc3e2cc6
U-AKELA\Roxanne <roxanne.coburn@gmail.com>
2017-04-23 11:17:40 -0400

    change scr

--- App/js/services/graphics-engine.js
+++ App/js/services/graphics-engine.js
@@ -21,7 +21,7 @@ angular.module("gameApp")
 
             blankScreen: function () {
                 var img = new Image;
-                img.src = "/App/img/bg.png";
+                img.src = "http://i.imgur.com/tRbwjj7.png";
                 var pat = this.canvas.createPattern(img, "repeat");
 
 
